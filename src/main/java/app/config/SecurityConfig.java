8dad44f4dd029bff755e019441c75d901c605de0
Gabriel <gab.mos92@gmail.com>
2017-03-25 13:07:57 +0100

    Added database user authentication

--- src/main/java/app/config/SecurityConfig.java
+++ src/main/java/app/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package app.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,12 +9,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+import javax.sql.DataSource;
+
 /**
  * Created by Gabriel on 20.03.2017.
  */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // need to set proper filter to not have problems with encoding, CharacterEncodingFilter must go first
@@ -47,7 +54,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-        // TODO
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource);
     }
 }
